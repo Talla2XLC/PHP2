c9e94a2b0e13a7f223b7729d4ec22e9c22691652
Talla2XLC <mmm-appp@mail.ru>
2019-12-16 22:21:12 +0300

    Initial commit

---
+++ autoload.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once 'Twig/Autoloader.php';
+Twig_Autoloader::register();
+
+spl_autoload_register("gbStandardAutoload");
+
+function gbStandardAutoload($className) {
+	$dirs = [
+		'c',
+		'm',
+	];
+	$found = false;
+	foreach ($dirs as $dir) {
+		$fileName = __DIR__ . '/' . $dir . '/' . $className . '.php';
+		if (is_file($fileName)) {
+
+			require_once $fileName;
+			$found = true;
+		}
+	}
+
+	if (!$found) {
+		throw new Exception('Unable to load ' . $className);
+	}
+	return true;
+}
+
+//$object = new Test;
